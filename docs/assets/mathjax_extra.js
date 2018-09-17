a137dca397654b52d5c312b103be6c5ac4f2bfc1
Ivan C <ivanyschen@berkeley.edu>
2018-09-17 14:33:51 -0700

    initialize project

---
+++ docs/assets/mathjax_extra.js
@@ -0,0 +1,22 @@
+// Enable Tex inline with $...$
+
+window.MathJax = {
+  tex2jax: {
+    inlineMath: [ ["$","$"], ["\\(","\\)"] ],
+    displayMath: [ ["\\[","\\]"] ]
+  },
+  TeX: {
+    TagSide: "right",
+    TagIndent: ".8em",
+    MultLineWidth: "85%",
+    equationNumbers: {
+      autoNumber: "AMS",
+    },
+    unicode: {
+      fonts: "STIXGeneral,'Arial Unicode MS'"
+    }
+  },
+  displayAlign: "center",
+  showProcessingMessages: false,
+  messageStyle: "none"
+};
